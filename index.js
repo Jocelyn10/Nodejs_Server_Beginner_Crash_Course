2b6f4c0a4774e659683c739aeccb2d8f151c78fb
Jocelyn10 <jocelyn@kinshasadigital.com>
2020-12-23 11:49:18 +0100

    fix port variable

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const news = require("./routes/news");
 app.use("/news", news);
 
 // Listenings at port:
-port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Server running at port:" + port);
 });
